e298607817934f734bb63fbb18beed4319d63216
Tom Gray <tomg77@protonmail.com>
2022-08-06 22:46:13 +0100

    Initial commit

---
+++ src/test/java/com/gray/mkchromecastweb/MkchromecastWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gray.mkchromecastweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkchromecastWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
